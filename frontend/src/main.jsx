99965447222a47a20c1fdfcceae0bfb88cbcac67
Kush <goyal.kush@hotmail.com>
2025-09-07 21:09:42 +0530

    smooth scroll to top

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import ScrollToTop from "./components/ScrollToTop.jsx";
 import { BrowserRouter } from "react-router-dom";
-
 import { ThemeProvider } from "@material-tailwind/react";
 
 createRoot(document.getElementById("root")).render(
   <ThemeProvider>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </ThemeProvider>
